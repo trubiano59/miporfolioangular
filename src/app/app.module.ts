865d8c7912bcb784df3a7804c536949ff3b164e4
Miguel Trubiano <trubiano59@hotmail.com>
2022-09-08 00:11:17 -0300

    arreglos en general a toda la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,12 @@ import { ProyectosComponent } from './proyectos/proyectos.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
-import { IndexComponent } from './index/index.component';
 import { NavbardashboardComponent } from './navbardashboard/navbardashboard.component';
 import { BotonlogautComponent } from './botonlogaut/botonlogaut.component';
 import { MainComponent } from './main/main.component';
 import { AcercademiComponent } from './acercademi/acercademi.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { IndexComponent } from './index/index.component';
 
 
 
@@ -44,12 +44,12 @@ import { CursosComponent } from './cursos/cursos.component';
     ProyectosComponent,
     DashboardComponent,
     ErrorComponent,
-    IndexComponent,
     NavbardashboardComponent,
     BotonlogautComponent,
     MainComponent,
     AcercademiComponent,
     CursosComponent,
+    IndexComponent,
     
 
    
