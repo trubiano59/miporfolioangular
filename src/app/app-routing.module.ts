b30aad7f70ff20af5807b0182454e1371fc6df98
Miguel Trubiano <trubiano59@hotmail.com>
2022-09-12 21:48:34 -0300

    arreglos por mal funcionamiento index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
+import { IndexComponent } from './index/index.component';
 
 
 const routes: Routes=[
-  {path:'',redirectTo:'/index', pathMatch:'full'},
-  {path: "dashboard",component:DashboardComponent},
-  {path:'**',component:ErrorComponent},
+  {path: "index", component:IndexComponent},
+  {path: "dashboard", component:DashboardComponent},
+  {path:'',redirectTo:'/Index', pathMatch:'full'},
+  {path:'**', component:ErrorComponent}
 ];
 
 
 @NgModule({
-  declarations: [],
+  /*declarations: [],*/
   imports: [RouterModule.forRoot(routes)],
   exports:[RouterModule]
 })
